f17a01ced79ec6104b8c85add7e357e16000a333
florincaciur <florincaciur1@gmail.com>
2026-05-08 15:15:08 +0300

    Update Microsoft Clarity project id

--- tools/generate-seo-blog-article.js
+++ tools/generate-seo-blog-article.js
@@ -16,9 +16,9 @@ const CLARITY_TRACKING_CODE = `  <!-- Clarity tracking code for https://atelierd
   <script>
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "wnxxdlfpac");
+    })(window, document, "clarity", "script", "wnzyco6rq");
   </script>`;
 
 const OFFICIAL_DOMAINS = [
